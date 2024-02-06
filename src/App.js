c75b7d2ad625467aa846b6b93bcff215070b69d8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-06 18:05:59 +0900

    feat: 테스트에 대응하는 실제 코드 작성

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import React, { useState } from "react";
 function App() {
 
   const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{count}</h3>
         <div>
-          <button data-testid="minus-button" onClick={() => setCount((count) => count - 1)}>-</button>
-          <button data-testid="plus-button" onClick={() => setCount((count) => count + 1)}>+</button>
+          <button data-testid="minus-button" disabled={disabled} onClick={() => setCount((count) => count - 1)}>-</button>
+          <button data-testid="plus-button" disabled={disabled} onClick={() => setCount((count) => count + 1)}>+</button>
+        </div>
+        <div>
+          <button style={{ backgroundColor: "blue" }} data-testid="on/off-button" onClick={() => setDisabled((prev) => !prev)}>on/off</button>
         </div>
       </header>
     </div>
