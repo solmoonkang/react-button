07f5dc013d7241e3bf204a5b82d3453f77b95b11
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-06 17:46:53 +0900

    feat: +버튼과 -버튼 테스트 코드 작성

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('the counter starts at 0', () => {
+test("the counter starts at 0", () => {
   render(<App />);
   const counteElement = screen.getByTestId("counter");
   expect(counteElement).toHaveTextContent(0);
+});
+
+test("minus button has correct text", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  expect(buttonElement).toHaveTextContent("-");
 });
